0f975102bed9dd35655b9700a9079a645fad849a
TAKAHASHI,Toru <torutk@gmail.com>
2017-04-24 06:26:57 +0900

    空のウィンドウを表示するJavaFX FXMLアプリケーション

---
+++ TinyGadget/src/tinygadget/TinyGadgetViewController.java
@@ -0,0 +1,21 @@
+/*
+ * © 2017 TAKAHASHI,Toru
+ */
+package tinygadget;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ *
+ * @author toru
+ */
+public class TinyGadgetViewController implements Initializable {
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
