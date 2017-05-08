a9c832a0056ea9ca34b57f8bbb82b5e0ba59fbc5
TAKAHASHI,Toru <torutk@gmail.com>
2017-05-09 00:09:49 +0900

    Ctrlキー＋ホイール操作でウィンドウの大きさを変える
    * sceneの大きさ（初期値）を暫定指定

--- TinyGadget/src/tinygadget/TinyGadgetApp.java
+++ TinyGadget/src/tinygadget/TinyGadgetApp.java
@@ -24,7 +24,7 @@ public class TinyGadgetApp extends Application {
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("TinyGadgetView.fxml"));
         
-        Scene scene = new Scene(root);
+        Scene scene = new Scene(root, 320, 200);
         
         // ウィンドウ枠の非表示と背景透明化
         scene.setFill(Color.TRANSPARENT);
@@ -40,10 +40,27 @@ public class TinyGadgetApp extends Application {
             stage.setY(event.getScreenY() - dragStartY);
         });
         
+        // マウスホイール操作でウィンドウの大きさを変更
+        scene.setOnScroll(event -> {
+            if (event.isControlDown()) {
+                zoom(event.getDeltaY() > 0 ? 1.1 : 0.9, stage);
+            }
+        });
         stage.setScene(scene);
         stage.show();
     }
 
+    /**
+     * 指定した拡大率で指定した stage の大きさを変更する。
+     * 
+     * @param factor 拡大率（1.0が等倍で、 1.0 より大で大きく、 1.0　より小で小さくする）
+     * @param stage 大きさを変更する対象
+     */
+    private void zoom(double factor, Stage stage) {
+        stage.setWidth(stage.getWidth() * factor);
+        stage.setHeight(stage.getHeight() * factor);
+    }
+    
     /**
      * @param args the command line arguments
      */
--- TinyGadget/src/tinygadget/TinyGadgetViewController.java
+++ TinyGadget/src/tinygadget/TinyGadgetViewController.java
@@ -5,17 +5,25 @@ package tinygadget;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.beans.binding.Bindings;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.Region;
+import javafx.scene.shape.Circle;
 
 /**
- *
+ * 
  * @author toru
  */
 public class TinyGadgetViewController implements Initializable {
+    @FXML
+    private Region root;
+    @FXML
+    private Circle circle;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        circle.radiusProperty().bind(Bindings.min(root.widthProperty(), root.heightProperty()).divide(2));
     }    
     
 }
