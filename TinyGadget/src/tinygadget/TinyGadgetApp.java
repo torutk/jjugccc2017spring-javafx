cfb9b0f3304bc1f3d2af5488016dc0c3117bfb20
TAKAHASHI,Toru <torutk@gmail.com>
2017-05-08 21:43:03 +0900

    ウィンドウ枠を非表示、背景を透明化

--- TinyGadget/src/tinygadget/TinyGadgetApp.java
+++ TinyGadget/src/tinygadget/TinyGadgetApp.java
@@ -7,7 +7,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  *
@@ -20,6 +22,10 @@ public class TinyGadgetApp extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("TinyGadgetView.fxml"));
         
         Scene scene = new Scene(root);
+        
+        // ウィンドウ枠の非表示と背景透明化
+        scene.setFill(Color.TRANSPARENT);
+        stage.initStyle(StageStyle.TRANSPARENT);
         
         stage.setScene(scene);
         stage.show();
