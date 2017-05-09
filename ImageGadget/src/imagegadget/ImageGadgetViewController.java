ac6299d185c5f5c836d7973d35a759d58941c0cb
TAKAHASHI,Toru <torutk@gmail.com>
2017-05-10 06:34:26 +0900

    ImageGadgetのNetBeans JavaFX FXMLアプリケーション雛形生成直後

---
+++ ImageGadget/src/imagegadget/ImageGadgetViewController.java
@@ -0,0 +1,33 @@
+/*
+ * © 2017 TAKAHASHI,Toru
+ */
+package imagegadget;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author toru
+ */
+public class ImageGadgetViewController implements Initializable {
+    
+    @FXML
+    private Label label;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
