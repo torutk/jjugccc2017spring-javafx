8a2e61dc10cab7666616ca4961cd3384ca48682f
TAKAHASHI,Toru <torutk@gmail.com>
2017-05-11 06:56:17 +0900

    ドラッグ＆ドロップで指定したディレクトリ以下の画像ファイルを順次表示
    * デフォルトはカレントディレクトリ以下
    * 制約：起動時10秒間は何も表示されない
    * 制約：リストを表示し終わると最後の画像が表示されたまま

--- ImageGadget/src/imagegadget/ImageGadgetViewController.java
+++ ImageGadget/src/imagegadget/ImageGadgetViewController.java
@@ -4,11 +4,19 @@
 package imagegadget;
 
 import java.net.URL;
+import java.nio.file.Paths;
 import java.util.ResourceBundle;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
 import javafx.scene.layout.Region;
+import javafx.util.Duration;
 
 /**
  * ImageGadgeViewのコントローラクラス。
@@ -20,11 +28,46 @@ public class ImageGadgetViewController implements Initializable {
     @FXML
     private ImageView imageView;
     
+    private ImageGadgetViewModel model = new ImageGadgetViewModel(Paths.get("."));
+    
+    @FXML
+    private void handleDragOver(DragEvent event) {
+        Dragboard dragboard = event.getDragboard();
+        if (dragboard.hasFiles() && dragboard.getFiles().get(0).isDirectory()) {
+            event.acceptTransferModes(TransferMode.ANY);
+        }
+        event.consume();
+    }
+    @FXML
+    private void handleDragDropped(DragEvent event) {
+        Dragboard dragboard = event.getDragboard();
+        if (dragboard.hasFiles() && dragboard.getFiles().get(0).isDirectory()) {
+            model = new ImageGadgetViewModel(Paths.get(dragboard.getFiles().get(0).toURI()));                
+            event.setDropCompleted(true);
+        } else {
+            event.setDropCompleted(false);
+        }
+        event.consume();
+    }
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         imageView.fitWidthProperty().bind(root.widthProperty());
         imageView.fitHeightProperty().bind(root.heightProperty());
         imageView.setPreserveRatio(true);
-    }    
+        
+        Timeline timer = new Timeline();
+        timer.getKeyFrames().add(new KeyFrame(Duration.seconds(10), this::nextImage));
+        timer.setCycleCount(Timeline.INDEFINITE);
+        timer.play();
+    }
     
+    private ImageGadgetViewModel getModel() {
+        return model;
+    }
+
+    private void nextImage(Event ev) {
+        getModel().next().ifPresent(imageView::setImage);
+    }
+       
 }
---
+++ ImageGadget/src/imagegadget/ImageGadgetViewModel.java
@@ -0,0 +1,61 @@
+/*
+ * © 2017 TAKAHASHI,Toru
+ */
+package imagegadget;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.PathMatcher;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import javafx.scene.image.Image;
+
+/**
+ * 指定されたパス（ディレクトリ）以下の画像ファイルから画像（Image）を順次取り出すイテレータのようなクラス。
+ * <p>
+ * <li>初期化時に、指定パス以下を再帰的に検索し画像ファイルのリストを作成（名前順ソート）
+ * <li>nextメソッドで順次画像（Image）オブジェクトを生成して返却
+ */
+public class ImageGadgetViewModel {
+
+    private Path folder;
+    private List<Path> imageFiles;
+    private int currentIndex;        
+    
+    public ImageGadgetViewModel(Path folder) {
+        if (Files.isDirectory(folder)) {
+            this.folder = folder;
+        }
+        imageFiles = new ArrayList<Path>();
+        folderData();
+    }
+    
+    public Optional<Image> next() {
+        if (imageFiles.size() <= currentIndex + 1) {
+            return Optional.empty();
+        }
+        return Optional.ofNullable(loadImage(imageFiles.get(++currentIndex)));
+    }
+
+    private Image loadImage(Path path) {
+        return new Image(path.toFile().toURI().toString());
+    }
+    
+    private void folderData() {
+        try (Stream<Path> stream = Files.walk(folder)) {
+            PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:**.{bmp,gif,jpeg,jpg,png}");
+            imageFiles = stream.filter(Files::isRegularFile)
+                    .filter(matcher::matches)
+                    .sorted()
+                    .collect(Collectors.toList());
+        } catch (IOException ex) {
+            throw new UncheckedIOException(ex);
+        }
+    }
+}
